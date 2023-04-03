3be8392642bf2730cf23251de1455226a1f61706
ikravchenko <ikravchenko@smartru.com>
2023-04-03 18:23:56 +0300

    palindrome number todo

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,13 @@
+@SuppressWarnings("UnusedReturnValue")
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        new PalindromeNumber().isPalindrome(121);
+    }
+    public boolean isPalindrome(int x) {
+        boolean res = false;
+        System.out.println("x = " + x);
+        // TODO
+        System.out.println("res = " + res);
+        return res;
+    }
+}
